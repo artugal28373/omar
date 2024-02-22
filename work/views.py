260dbb159474c1377f76b9353306cc09fdbd426b
artugal28373 <58047037+artugal28373@users.noreply.github.com>
2024-02-23 01:36:02 +0600

    Update views.py

--- work/views.py
+++ work/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 # Create your views here.
-@api_view(["GET"])
 def omar(request):
     return render(request, 'omarView.html')
